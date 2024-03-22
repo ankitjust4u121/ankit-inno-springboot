bba181067072ca264d9fb4b7c84cf1db8da45a6f
Ankit Upadhyay <ankitjust4u121@gmail.com>
2024-03-22 22:48:56 +0530

    This is just Inno Demo with Git.

---
+++ src/main/java/com/ankit/InnoDemo/InnoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ankit.InnoDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnoDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ankit/InnoDemo/InnoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.InnoDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
